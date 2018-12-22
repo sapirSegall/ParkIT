937a7360f61ad0b668cfe59b85b997e93d6dcc64
sapirSegall <sap456@walla.com>
2018-12-22 12:53:47 +0200

    psh all the lab4_ex files

---
+++ lab4_ex/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title>Form Example</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+      Username: <asp:TextBox runat="server" Id="username"></asp:TextBox><br/>
+        Password: <input type="password" name="password"/>
+    </div>
+        <div>
+            <asp:Button runat="server" Text="Submit" OnClick="OnClick"/>
+        </div>
+        <div>
+            <asp:Label runat="server" id="result"></asp:Label>
+        </div>
+        <div><%=Result %></div>
+        
+    </form>
+</body>
+</html>
---
+++ lab4_ex/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public string Result = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+    }
+
+    protected void OnClick(object sender, EventArgs e)
+    {
+        var user = username.Text;        
+        var user2 = Convert.ToString(Request["username"]);
+        var pas = Convert.ToString(Request["password"]);
+
+        Result = String.Format("User: {0}, password: {1}", user, pas);
+        result.Text = Result;
+        //Response.Write(Result);
+
+    }
+}
