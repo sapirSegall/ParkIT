eb77ff20edcaf0a719fd8a4b8c414cee1ff79036
talhassid <talhassidd@gmail.com>
2019-01-25 17:12:24 +0200

    sort fix

--- lab4_ex/requestsDynamicTable.js
+++ lab4_ex/requestsDynamicTable.js
@@ -130,8 +130,8 @@ function sortTable() {
       shouldSwitch = false;
       /*Get the two elements you want to compare,
       one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[3];
-      y = rows[i + 1].getElementsByTagName("TD")[3];
+      x = rows[i].getElementsByTagName("TD")[2];
+      y = rows[i + 1].getElementsByTagName("TD")[2];
       //check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
         //if so, mark as a switch and break the loop:
